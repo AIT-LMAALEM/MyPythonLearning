8f46795a1465e2cccaef6488812bf12618be2548
AIT-LMAALEM <mhamed.aitlmaalem13@gmail.com>
2025-08-03 21:48:39 +0200

    création d'un ensemble

---
+++ basics/sets/exercice/exercice01.py
@@ -0,0 +1,23 @@
+# un ensemble(set)  {,,,}
+# les éléments d'un ensemble ne sont pas ordonnés
+amis={"imad","rachid","samir","aymen"}
+print(amis)
+#les elements d'un ensembles ne sont pas indexés
+#les elements d'un ensembles ne peuvent pas être modifiés.mais on peut ajouter ou supprimer des elements apres sa creation
+#les elements d'un ensembles sont uniques
+num={1,1,1,2,3,3,4}
+print(num)
+#un ensemble peut contenir différents types de données(int,float,string,bool)
+etudiant={"rida rachidi",19,174.5}
+print(etudiant)
+
+#creation
+E=set(("hello","bonjour","salut","marhaba"))
+print(E)
+F=set(range(1,20,2))
+print(F)
+D={2,5,1,0,18,23,11,20}
+print(D)
+
+#affichage
+#print(nomSet)
