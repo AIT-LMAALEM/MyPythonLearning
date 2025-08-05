d9f4a829a95ac75fccf2d72044ad0d3448b0a6d5
AIT-LMAALEM <mhamed.aitlmaalem13@gmail.com>
2025-08-05 22:55:47 +0200

    supprimer espaces + afficher nombre de caractères

--- basics/sets/exercice/exercice05.py
+++ basics/sets/exercice/exercice05.py
@@ -5,12 +5,31 @@
 #         K.remove(i)
 # print(K)
 
+
+
 #programme qui compte le nombre de voyelles dans un texte
-voyelles={"a","e","i","o","u","y"}
-texte=input("entrer un texte:").lower()
-count=0
-for x in texte:
-    if x in voyelles:
-        count+=1
-print("le nombre de voyelles dans le texte est:",count)
+# voyelles={"a","e","i","o","u","y"}
+# texte=input("entrer un texte:").lower()
+# count=0
+# for x in texte:
+#     if x in voyelles:
+#         count+=1
+# print("le nombre de voyelles dans le texte est:",count)
+
+
+#programme qui affiche les mots qui composent un texte saisi par l'utilisateur
+# texte1=input("entrer un texte:").split(" ")
+# print("les mots qui composent le texte sont:")
+# print(set(texte1))
+
+#programme qui affiche les mots qui composent un texte saisi par l'utilisateur
+texte2=input("entrer un texte:")
+list_texte2=texte2.split()
+sans_espace=""
+for i in list_texte2:
+    sans_espace+=i
+print("les caractères qui composent le texte est:",set(sans_espace))
+
+    
+
 
