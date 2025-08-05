51ec4f720be3979438cc7ef5da40bc8a3e37e2b8
AIT-LMAALEM <mhamed.aitlmaalem13@gmail.com>
2025-08-05 22:29:36 +0200

    le nombre de voyelles dans un texte

--- basics/sets/exercice/exercice05.py
+++ basics/sets/exercice/exercice05.py
@@ -1,6 +1,16 @@
 #programme qui supprime tous les nombres entiers d'un ensemble
-K={11 , 1.5 , 4 , 3.14 , 16 , 1.42}
-for i in list(K):
-    if type(i)==int:
-        K.remove(i)
-print(K)
+# K={11 , 1.5 , 4 , 3.14 , 16 , 1.42}
+# for i in list(K):
+#     if type(i)==int:
+#         K.remove(i)
+# print(K)
+
+#programme qui compte le nombre de voyelles dans un texte
+voyelles={"a","e","i","o","u","y"}
+texte=input("entrer un texte:").lower()
+count=0
+for x in texte:
+    if x in voyelles:
+        count+=1
+print("le nombre de voyelles dans le texte est:",count)
+
