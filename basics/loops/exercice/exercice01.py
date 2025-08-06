6e5fc959f503c22fc6da0338d1a173f928c826b1
AIT-LMAALEM <mhamed.aitlmaalem13@gmail.com>
2025-08-06 22:31:57 +0200

    exercice01.

--- basics/loops/exercice/exercice01.py
+++ basics/loops/exercice/exercice01.py
@@ -1,18 +1,18 @@
-# names=input("Enter the first and the last name of your friends separated by a comma:").split(", ")
-# abriv_list=[]
-# for name in names:
-#     name_p=name.split()
-#     print(name_p)
-#     first_name=name_p[0]
-#     seconde_name=name_p[1]
-#     abriv=first_name[0]+"."+seconde_name[0]+"."
-#     abriv_list.append(abriv)
+names=input("Enter the first and the last name of your friends separated by a comma:").split(", ")
+abriv_list=[]
+for name in names:
+    name_p=name.split()
+    print(name_p)
+    first_name=name_p[0]
+    seconde_name=name_p[1]
+    abriv=first_name[0]+"."+seconde_name[0]+"."
+    abriv_list.append(abriv)
 
-# for i in abriv_list:
-#     print(i)
-
-
+for i in abriv_list:
+    print(i)
 
 
+
+#reverste sentence
 sentence=input("Eenter a sentence:")
-print("h".join(sentence[::-1]))
+print(sentence[::-1])
