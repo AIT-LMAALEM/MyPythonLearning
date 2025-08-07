8a22569b0e8c496a760b4f0c4a68c952b9ebba52
AIT-LMAALEM <mhamed.aitlmaalem13@gmail.com>
2025-08-07 23:25:17 +0200

    hangman 3

--- basics/functions/project1/main.py
+++ basics/functions/project1/main.py
@@ -4,16 +4,36 @@ import random
 words=["office","panda","bad","ginger"]
 random_word=random.choice(words)
 display=[]
-
-
 for i in range(len(random_word)):
     display.append("_")
     print("_",end=" ")
-guessed=input("\nguess a letter:").lower()
 
-for j in random_word:
-    if guessed==j:
-        display[random_word.index(j)]=guessed
+lives=6
+while "_" in display and lives >0:
+    guessed=input("\nguess a letter:").lower()
+    if guessed not in random_word:
+        lives-=1
 
-for i in display:
-    print(i,end=" ")
+    for j in range(len(random_word)):
+         if guessed==random_word[j]:
+           display[j]=guessed
+
+    for i in display:
+        print(i,end=" ")
+
+    print("\nyou have ",lives," more tries")
+    
+
+if lives==0:
+    print("\n ** you lose** \n")
+    print(   '''
+      +---+
+      |   |
+      O   |
+     /|\  |
+     / \  |
+          |
+     =========''')
+else:
+    print("\n** you Win **\n")
+
