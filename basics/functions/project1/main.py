00bcb511300f2315bc88f401afad6870ece51e89
AIT-LMAALEM <mhamed.aitlmaalem13@gmail.com>
2025-08-07 22:45:36 +0200

    hangman 2

--- basics/functions/project1/main.py
+++ basics/functions/project1/main.py
@@ -1,10 +1,19 @@
+# hangman game
 import random 
 
-words=["office","panda","cabin","ginger"]
+words=["office","panda","bad","ginger"]
 random_word=random.choice(words)
-letter=input("please guess a letter:")
-for i in random_word:
-    if i==letter:
-        print("Right")
-    else:
-        print("Wrong")
+display=[]
+
+
+for i in range(len(random_word)):
+    display.append("_")
+    print("_",end=" ")
+guessed=input("\nguess a letter:").lower()
+
+for j in random_word:
+    if guessed==j:
+        display[random_word.index(j)]=guessed
+
+for i in display:
+    print(i,end=" ")
