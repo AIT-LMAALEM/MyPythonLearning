24f505151a96e7b69b727d3a9e5dbbbdff89afa5
AIT-LMAALEM <mhamed.aitlmaalem13@gmail.com>
2025-08-08 22:52:33 +0200

    last hangman

--- basics/functions/project1/main.py
+++ basics/functions/project1/main.py
@@ -1,39 +1,99 @@
 # hangman game
 import random 
 
+HANGMANPICS = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+
 words=["office","panda","bad","ginger"]
 random_word=random.choice(words)
-display=[]
-for i in range(len(random_word)):
-    display.append("_")
+display=["_"]*len(random_word)
 print(" ".join(display))
-
-# en peut aussi faire : display=["_"]*len(random_word)   sans boucle for
-
 lives=6
+print(HANGMANPICS[0])
+
+guessed_letter=[]
 while "_" in display and lives >0:
     guessed=input("guess a letter:").lower()
+
+    if guessed in guessed_letter:
+        print("you already guessed that. Try again:")
+        print(f"you have {lives} more tries")
+        continue
+    guessed_letter.append(guessed)
+
+
+
     if guessed not in random_word:
         lives-=1
-
-    for j in range(len(random_word)):
-         if guessed==random_word[j]:
-           display[j]=guessed
+        print(HANGMANPICS[6-lives])
+    else:
+        for j in range(len(random_word)):
+            if guessed==random_word[j]:
+               display[j]=guessed
 
     print(" ".join(display))
-    print("you have ",lives," more tries\n")
+    print("\nyou have ",lives," more tries")
     
 
 if lives==0:
     print("\n ** you lose** \n")
-    print(   '''
-      +---+
-      |   |
-      O   |
-     /|\  |
-     / \  |
-          |
-     =========''')
+   
 else:
     print("\n** you Win **\n")
+
+
+
+
+
+
+
 
