f5ced4ff4f0a2141baf43879b969a2d36bee38d9
AIT-LMAALEM <mhamed.aitlmaalem13@gmail.com>
2025-08-08 23:17:41 +0200

    argument function

---
+++ basics/functions/exercice/exercice02.py
@@ -0,0 +1,9 @@
+# function with parameters
+def hi(name):
+    print("hello",name)
+name=input("enter your name:")
+hi(name)
+
+def information(name,age,nationality):
+    print(f"Your name is {name}, and you are {age} years old, you are {nationality}")
+information("mhamed",19,"marocaine")
