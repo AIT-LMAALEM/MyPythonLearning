ba4ac930ef52ac49b2990801e633c1366de4d098
AIT-LMAALEM <mhamed.aitlmaalem13@gmail.com>
2025-08-09 22:06:57 +0200

    encrypt function

--- basics/functions/project2/main.py
+++ basics/functions/project2/main.py
@@ -1,20 +1,22 @@
-# encrypted sentence
 import string
-alphabet=string.ascii_lowercase
-sentence= input("Please type a sentence:").lower()
-step=int(input("enter the step:"))
-encrypted_sentence=""
-for letter in sentence:
-    if letter in alphabet:
-        original_position=alphabet.index(letter)
-        new_position= (original_position +step)%26
-        encrypted_sentence+=alphabet[new_position]
-    else:
-        encrypted_sentence+=" "
-print("Here is the encrypted sentence: ",encrypted_sentence)
 
-        
+def encrypt(message, shift):
+    alphabet1 = string.ascii_lowercase
+    alphabet2 = string.ascii_uppercase
+    encrypted_message = ""
+    for letter in message:
+        if letter in alphabet1:
+            original_position = alphabet1.index(letter)
+            new_position = (original_position + shift) % 26
+            encrypted_message += alphabet1[new_position]
+        elif letter in alphabet2:
+            original_position = alphabet2.index(letter)
+            new_position = (original_position + shift) % 26
+            encrypted_message += alphabet2[new_position]
+        else:
+            encrypted_message += letter
+    print("Here is the encrypted message:", encrypted_message)
 
-
-
-
+message = input("Enter a message: ")
+shift = int(input("Enter a shift number: "))
+encrypt(message, shift)
