6bc14271ddf9a11785e4fe9df3cc902bcfd12983
AIT-LMAALEM <mhamed.aitlmaalem13@gmail.com>
2025-08-11 23:21:27 +0100

    exemples comprehension list

---
+++ basics/list/list comprehension/file02.py
@@ -0,0 +1,26 @@
+fruits=["apple","banana","cherry","kiwi","mango"]
+fruits_a=[]
+for fruit in fruits:
+    if "a" in fruit:
+        fruits_a.append(fruit)
+print(fruits_a)
+
+#methode 2
+fruits_a=[  fruit for fruit in fruits if "a" in fruit]
+print(fruits_a)
+
+# exe1
+numbers=[1,2,3,4,5,6,7,8,9,10]
+print(["even" if x%2==0 else "odd" for x in numbers])
+
+
+#exe2
+grades=[20,55,27,70,82,19,90]
+result=["pass" if i >=50 else "fail" for i in grades]
+for grad, res in zip(grades,result):
+    print(f"{grad} :{res}")
+
+#exe3
+words=["on","apple","holdBack","braveMan","today"]
+words_l=[word.upper() if len(word)<=3 else word.lower() for word in words]
+print(words_l)
