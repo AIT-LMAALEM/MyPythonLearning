026ee84139696a16197f27e6cd5832253f531903
AIT-LMAALEM <mhamed.aitlmaalem13@gmail.com>
2025-08-12 21:09:16 +0100

    lambda function

---
+++ basics/functions/exercice/lambda_f.py
@@ -0,0 +1,16 @@
+# fuction => Lambda 
+# Anonymous function
+# 1 it has no name 
+# 2 lambda is one single expression not block of code
+
+def say_hello(name):
+    return f" hello {name}"
+
+hello= lambda name:f"hello {name}"
+
+print(hello("ahmed"))
+print(say_hello("ahmed"))
+
+
+
+
