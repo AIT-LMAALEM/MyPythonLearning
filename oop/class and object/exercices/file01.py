ed649c3ca15b951cce11916fe3ea27f898c563fe
AIT-LMAALEM <mhamed.aitlmaalem13@gmail.com>
2025-08-13 18:20:18 +0100

    class object attributes

---
+++ oop/class and object/exercices/file01.py
@@ -0,0 +1,19 @@
+class Book:
+    title = ""
+    auther = ""
+    pages = 0
+
+my_book= Book()
+my_book.title= "Origin"
+my_book.auther = "Dan brown"
+my_book.pages = 356
+print(  my_book.title)
+print(  my_book.auther)
+print(  my_book.pages)
+
+
+seconde_book= Book()
+seconde_book.title= "the 99"
+seconde_book.auther= "Paula Hawkins"
+seconde_book.pages=250
+
