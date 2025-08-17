b62f46e8850726332f64dcd3a2d890da53552d04
AIT-LMAALEM <mhamed.aitlmaalem13@gmail.com>
2025-08-17 22:55:10 +0100

    exemple animals

---
+++ oop/Inheritance/file02.py
@@ -0,0 +1,33 @@
+class Animal:
+
+    alive= True
+    def eat(self):
+        print("this animal is eating")
+    def sleep(self):
+        print("this animal is sleeping")
+
+
+class Rabbit(Animal):
+    def run(self):
+        print("this rabbit is running")
+
+class Fish(Animal):
+    def swim(self):
+        print("this fish is swimming")
+    
+class Hawk(Animal):
+    def fly(self):
+        print("this hawk is flying")
+    
+
+
+rabbit= Rabbit()
+fish =Fish()
+hawk=Hawk()
+
+print(rabbit.alive)
+print(fish.eat())
+print(hawk.sleep())
+
+print(rabbit.run())
+print(fish.swim())
