e05f727fa66c2d11627e1431593ab20d912aa4fc
AIT-LMAALEM <mhamed.aitlmaalem13@gmail.com>
2025-08-29 23:27:12 +0100

    skill app v-2

--- DataBase/SQLite/project/SkillsApp.py
+++ DataBase/SQLite/project/SkillsApp.py
@@ -1,4 +1,6 @@
 import sqlite3
+import os
+import time
 
 db=sqlite3.connect("AppSkill.db")
 Cr=db.cursor()
@@ -30,22 +32,54 @@ commands_list=["s","a","d","u","q"]
 user_id=1
 
 #Define Functions
+def clear_screen():
+    os.system("cls" if os.name=="nt" else "clear")
+    
 def show_skill():
+    clear_screen()
+    Cr.execute(f"SELECT * FROM skills WHERE user_id='{user_id}'")
+    results=Cr.fetchall()
+    print(f"You Have {len(results)}")
+    if len(results)>0:
+        print("Showing Skills With Progress")
+        time.sleep(1)
+        for row in results:
+            print(f" Skill => {row[0]},",end=" ")
+            print(f" Progress => {row[1]}")
     commit_and_close()
+
 
 def add_skill():
+    clear_screen()
     skill=input("Write Skill Name: ").strip().capitalize()
-    progress=input("Write Skill Progress: ").strip()
-    Cr.execute(f"INSERT INTO  skills(name, progress, user_id) values('{skill}','{progress}','{user_id}')")
-
+    Cr.execute(f"SELECT name FROM skills WHERE name='{skill}' AND user_id='{user_id}'")
+    results=Cr.fetchone()
+    if results !=None:
+        print("Skill Is Exists you Can't Add It")
+    else:
+        progress=input("Write Skill Progress: ").strip()
+        Cr.execute(f"INSERT INTO  skills(name, progress, user_id) values('{skill}','{progress}','{user_id}')")
+        time.sleep(1)
+        print("Skill Is Added ")
     commit_and_close()
+
 
 def delete_skill():
+    clear_screen()
     skill=input("Write Skill Name: ").strip().capitalize()
     Cr.execute(f"DELETE FROM skills WHERE name='{skill}' AND user_id='{user_id}'")
+    time.sleep(1)
+    print("Skill Is Deleted")
     commit_and_close()
 
+
 def update_skill():
+    clear_screen()
+    skill=input("Write Skill Name: ").strip().capitalize()
+    new_progress=input("Write The New Progress").strip()
+    Cr.execute(f"UPDATE skills SET progress ='{new_progress} WHERE user_id='{user_id}' AND name='{skill}'")
+    time.sleep(1)
+    print("Skill Is Updated")
     commit_and_close()
 
 
@@ -67,3 +101,20 @@ if user_input in commands_list:
 
 else:
     print(f"Sorry This Commands \"{user_input}\" Is Not Found")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
